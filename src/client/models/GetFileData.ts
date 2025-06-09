71823356db688f2496f2b1a5b675c110dafa2428
AlexSserb <aserbin190@gmail.com>
2025-06-10 00:08:50 +0400

    Api client types

---
+++ src/client/models/GetFileData.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GetFileData = {
+    base64String: string;
+    filename: string;
+    mimeType: string;
+};
+
